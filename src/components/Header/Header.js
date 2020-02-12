ddbd8be44f16935b8df29c5c2c781e373e073e48
Dan Birch <dan.birch@hermes-europe.co.uk>
2020-02-12 19:33:08 +0000

    Header work

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
+
 import {NavLink} from "react-router-dom";
 import "./Header.scss";
 import { ReactComponent as LogoSvg}
   from "../../assets/logos/SaveSwift.svg";
+import { ReactComponent as BurgerSvg}
+  from "../../assets/icons/burger.svg";
 const Header = () => {
+  
+  const [showLinks, setShowLinks] = useState(false);
+
+  function handleClick(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    setShowLinks(!showLinks);
+  }
+  function hideMenu(e){
+    e.stopPropagation();
+    setShowLinks(false);
+  }
   return (
-    <nav>
-      <LogoSvg className="main-logo" />
-      <div className="links">
-        <NavLink to="/"
-          className="text-link"
-          activeClassName='text-link--active'
-          exact>Home</NavLink>
-        <NavLink to={{pathname:"/Accounts"}}
-          exact
-          activeClassName='text-link--active'
-          className="text-link">Accounts</NavLink>
-      </div>
-    </nav>
+    <div>
+      <nav>
+        <div className="logos">
+          <LogoSvg className={"main-logo"} />
+          <BurgerSvg onClick={handleClick} className="burger-icon" />
+        </div>
+        <div className={`links ${showLinks ? "active" : ""}`}>
+          <NavLink to="/"
+            className="text-link"
+            activeClassName='text-link--active'
+            onClick={hideMenu}
+            exact>Home</NavLink>
+          <NavLink to={{pathname:"/Accounts"}}
+            exact
+            activeClassName='text-link--active'
+            onClick={hideMenu}
+            className="text-link">Accounts</NavLink>
+        </div>
+      </nav>
+    </div>
+
   );
 };
 
